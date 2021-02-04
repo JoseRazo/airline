cc38f6662f3096e403ead753c4b5ddb0d834921a
JoseRazo <jrazo@utsalamanca.edu.mx>
2021-02-04 16:09:58 -0600

    Testing CI con Travis y Django 3.0.4 y sqlite3

--- airline/settings.py
+++ airline/settings.py
@@ -93,12 +93,8 @@ DATABASES = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'biblioteca',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
